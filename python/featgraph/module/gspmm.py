eda55ad980037d6e78d3e1b0750d3ae155df4d00
Ubuntu <ubuntu@ip-172-31-2-188.us-east-2.compute.internal>
2020-07-02 07:20:02 +0000

    gspmm

--- python/featgraph/module/gspmm.py
+++ python/featgraph/module/gspmm.py
@@ -12,10 +12,15 @@ binary_op_map = {
     'mul': lambda x,y : x*y,
     'div': lambda x,y : x/y,
     'copy_u' : lambda x,y : x,
-    'copy_v' : lambda x,y : y,
+    'copy_e' : lambda x,y : y,
 }
-binary_ops = ['add', 'sub', 'mul', 'div', 'copy_u', 'copy_v', 'dot']
-# binary_ops = ['add', 'sub', 'mul', 'div', 'copy_u', 'copy_v']
+reduce_op_map = {
+    'sum' : te.sum,
+    'max' : te.max,
+    'min' : te.min
+}
+binary_ops = ['add', 'sub', 'mul', 'div', 'copy_u', 'copy_e']
+reduce_ops = ['sum', 'max', 'min']
 indice_types = ['int32', 'int64']
 feature_types = ['float32', 'float64']
 
@@ -24,7 +29,7 @@ def find_num_threads(l, ntx):
         return 1
     return te.if_then_else(ntx > l, find_num_threads(l, ntx >> 1), ntx)
 
-def gsddmm(binary_op, indice_type='int32', feature_type='float32', use_bcast=False, target='llvm'):
+def gspmm(binary_op, reduce_op, indice_type='int32', feature_type='float32', use_bcast=False, target='llvm'):
     # sparse matrix variables
     nnz = te.var('nnz', 'int64')
     num_rows = te.var('num_rows', indice_type)
@@ -33,47 +38,35 @@ def gsddmm(binary_op, indice_type='int32', feature_type='float32', use_bcast=Fal
     # dtype set to be the same as the other dim in the shape, i.e. nnz, num_rows, num_cols
     # otherwise cannot pass tvm type check 
     lhs_len = te.var('lhs_len', indice_type)
-    rhs_len = te.var('rhs_len', indice_type)
-    out_len = te.var('out_len', 'int64')
+    rhs_len = te.var('rhs_len', 'int64')
+    out_len = te.var('out_len', indice_type)
     # placeholder for sparse matrix
-    adj_row_indices = te.placeholder((nnz,), indice_type, 'adj_row_indices')
-    adj_col_indices = te.placeholder((nnz,), indice_type, 'adj_col_indices')
+    adj_indptr = te.placeholder((num_rows+1,), indice_type, 'adj_indptr')
+    adj_indices = te.placeholder((nnz,), indice_type, 'adj_indices')
+    edge_feat = te.placeholder((nnz, rhs_len), feature_type, 'edge_feat')
     # placeholder for dense features
-    src_feat = te.placeholder((num_rows, lhs_len), feature_type, 'src_feat')
-    dst_feat = te.placeholder((num_cols, rhs_len), feature_type, 'dst_feat')
+    node_feat = te.placeholder((num_cols, lhs_len), feature_type, 'src_feat')
     # placeholder for possible broadcasting offset
     lhs_off = te.placeholder((out_len,), indice_type, 'lhs_off')
     rhs_off = te.placeholder((out_len,), indice_type, 'rhs_off')
+    one = tvm.tir.IntImm(dtype=indice_type, value=1)
     # compute
-    if binary_op == 'dot':
-        reduce_size = te.var('reduce_size', indice_type)
-        k = te.reduce_axis((0, reduce_size), name='k')
-        out = te.compute(
-            (nnz, out_len),
-            lambda eid, fid: te.sum(
-                src_feat[adj_row_indices[eid], (lhs_off[fid] if use_bcast else fid) * reduce_size + k] * \
-                dst_feat[adj_col_indices[eid], (rhs_off[fid] if use_bcast else fid) * reduce_size + k],
-                axis=k
-            ),
-            name='out'
-        )
-    else:
-        out = te.compute(
-            (nnz, out_len), 
-            lambda eid, fid: binary_op_map[binary_op](
-                src_feat[adj_row_indices[eid], lhs_off[fid] if use_bcast else fid], \
-                dst_feat[adj_col_indices[eid], rhs_off[fid] if use_bcast else fid]
-            ),
-            name='out'
-        )
+    def msgfunc(row, fid):
+        row_start = adj_indptr[row]
+        # row_end = adj_indptr[tvm.tir.Add(row, tvm.tir.IntImm(dtype=row.dtype, value=1))]
+        row_end = adj_indptr[tvm.tir.Add(row, 1)]
+        row_num_elems = row_end - row_start
+        elem_idx = te.reduce_axis((0, row_num_elems), name="elem_idx")
+        adj_val = edge_feat[row_start + elem_idx, rhs_off[fid] if use_bcast else fid]
+        feat_val = node_feat[adj_indices[row_start + elem_idx], lhs_off[fid] if use_bcast else fid]
+        return reduce_op_map[reduce_op](binary_op_map[binary_op](feat_val, adj_val), axis=elem_idx)
+    out = te.compute((num_rows, out_len), msgfunc, name='out')
     # prepare input
-    f_input = [adj_row_indices, adj_col_indices, src_feat, dst_feat]
-    f_name = 'sddmm_{}_{}_{}'.format(binary_op, indice_type, feature_type)
+    f_input = [adj_indptr, adj_indices, node_feat, edge_feat]
+    f_name = 'spmm_{}_{}_{}_{}'.format(binary_op, reduce_op, indice_type, feature_type)
     if use_bcast:
         f_input += [lhs_off, rhs_off]
         f_name += '_bcast'
-    if binary_op == 'dot':
-        f_input.append(reduce_size)
     f_input.append(out)
     # schedule
     s = te.create_schedule(out.op)
@@ -90,26 +83,33 @@ def gsddmm(binary_op, indice_type='int32', feature_type='float32', use_bcast=Fal
         # s[out].bind(edge_outer, te.thread_axis('blockIdx.y'))
         # s[out].bind(edge_inner, te.thread_axis('threadIdx.y'))
         edge_axis, feat_axis = out.op.axis[0], out.op.axis[1]
+        reduce_axis = out.op.reduce_axis[0]
+        # single thread reduce
         edge_outer, edge_inner = s[out].split(edge_axis, factor=1024)
         s[out].bind(edge_outer, te.thread_axis('blockIdx.x'))
         s[out].bind(edge_inner, te.thread_axis('threadIdx.x'))
+        # tree reduce
+        # edge_outer, edge_inner = s[out].split(edge_axis, factor=128)
+        # s[out].bind(edge_outer, te.thread_axis('blockIdx.x'))
+        # s[out].bind(reduce_axis, te.thread_axis('threadIdx.x'))
     return tvm.lower(s, f_input, name=f_name)
 
-def sddmm():
+def spmm():
     pass
 
 def build_all(target, dir = None):
     # build a .so library packing all kinds of kernel for a target(cuda/llvm)
     modules = []
-    for op in binary_ops:
-        for id_type in indice_types:
-            for f_type in feature_types:
-                for ub in [True, False]:
-                    # print('Building sddmm_{}_{}_{}{}'.format(op, id_type, f_type, '_bcast' if ub else ''))
-                    modules.append(gsddmm(op, id_type, f_type, ub, target))
-    f = tvm.build(modules, target=target, name='gsddmm_' + target)
+    for b_op in binary_ops:
+        for r_op in reduce_ops:
+            for id_type in indice_types:
+                for f_type in feature_types:
+                    for ub in [True, False]:
+                        print('Building spmm_{}_{}_{}_{}{}'.format(b_op, r_op, id_type, f_type, '_bcast' if ub else ''))
+                        modules.append(gspmm(b_op, r_op, id_type, f_type, ub, target))
+    f = tvm.build(modules, target=target, name='gspmm_' + target)
     if dir:
-        f.export_library(dir + '/libgsddmm_tvm_' + target + '.so')
+        f.export_library(dir + '/libgspmm_tvm_' + target + '.so')
     return f
 
 
@@ -117,10 +117,10 @@ if __name__ == '__main__':
     import scipy, logging
     target = 'cuda'
     # f = build_all(target)
-    ir = gsddmm('add', indice_type='int32', feature_type='float32', target=target, use_bcast=False)
-    # print(ir)
+    ir = gspmm('add', 'sum', indice_type='int64', feature_type='float32', target=target, use_bcast=False)
+    print(ir)
     f = tvm.build(ir, target=target)
-    print(f.imported_modules[0].get_source())
+    # print(f.imported_modules[0].get_source())
     # adj_scipy_coo = scipy.sparse.random(2**15, 2**15, density=0.001, format='coo').astype('int32')
     # evaluate_time()
 
