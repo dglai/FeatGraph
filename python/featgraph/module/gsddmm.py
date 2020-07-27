41d17acc562085e8bbdc67164ddb0e6a7c39bf02
Ubuntu <ubuntu@ip-172-31-2-188.us-east-2.compute.internal>
2020-07-27 05:33:30 +0000

    remove general ones

--- python/featgraph/module/gsddmm.py
+++ python/featgraph/module/gsddmm.py
@@ -14,91 +14,8 @@ binary_op_map = {
     'copy_rhs' : lambda x,y : y,
 }
 binary_ops = ['add', 'sub', 'mul', 'div', 'copy_lhs', 'copy_rhs', 'dot']
-# binary_ops = ['add', 'sub', 'mul', 'div', 'copy_lhs', 'copy_rhs']
 indice_types = ['int32', 'int64']
 feature_types = ['float32', 'float64']
-
-def find_num_threads(l, ntx):
-    if ntx <= 1:
-        return 1
-    return te.if_then_else(ntx > l, find_num_threads(l, ntx >> 1), ntx)
-
-def gsddmm(binary_op, indice_type='int32', feature_type='float32', use_bcast=False, target='llvm'):
-    # sparse matrix variables
-    nnz = te.var('nnz', 'int64')
-    num_rows = te.var('num_rows', indice_type)
-    num_cols = te.var('num_cols', indice_type)
-    # feature length variables
-    # dtype set to be the same as the other dim in the shape, i.e. nnz, num_rows, num_cols
-    # otherwise cannot pass tvm type check 
-    lhs_len = te.var('lhs_len', indice_type)
-    rhs_len = te.var('rhs_len', indice_type)
-    out_len = te.var('out_len', 'int64')
-    # placeholder for sparse matrix
-    adj_row_indices = te.placeholder((nnz,), indice_type, 'adj_row_indices')
-    adj_col_indices = te.placeholder((nnz,), indice_type, 'adj_col_indices')
-    # placeholder for dense features
-    src_feat = te.placeholder((num_rows, lhs_len), feature_type, 'src_feat')
-    dst_feat = te.placeholder((num_cols, rhs_len), feature_type, 'dst_feat')
-    # placeholder for possible broadcasting offset
-    lhs_off = te.placeholder((out_len,), indice_type, 'lhs_off')
-    rhs_off = te.placeholder((out_len,), indice_type, 'rhs_off')
-    # compute
-    if binary_op == 'dot':
-        reduce_size = te.var('reduce_size', indice_type)
-        k = te.reduce_axis((0, reduce_size), name='k')
-        out = te.compute(
-            (nnz, out_len),
-            lambda eid, fid: te.sum(
-                src_feat[adj_row_indices[eid], (lhs_off[fid] if use_bcast else fid) * reduce_size + k] * \
-                dst_feat[adj_col_indices[eid], (rhs_off[fid] if use_bcast else fid) * reduce_size + k],
-                axis=k
-            ),
-            name='out'
-        )
-    else:
-        out = te.compute(
-            (nnz, out_len), 
-            lambda eid, fid: binary_op_map[binary_op](
-                src_feat[adj_row_indices[eid], lhs_off[fid] if use_bcast else fid], \
-                dst_feat[adj_col_indices[eid], rhs_off[fid] if use_bcast else fid]
-            ),
-            name='out'
-        )
-    # prepare input
-    f_input = [adj_row_indices, adj_col_indices]
-    f_name = 'sddmm_{}_{}_{}'.format(binary_op, indice_type, feature_type)
-    if binary_op == 'copy_lhs':
-        f_input.append(src_feat)
-    elif binary_op == 'copy_rhs':
-        f_input.append(dst_feat)
-    else:
-        f_input += [src_feat, dst_feat]
-    if binary_op == 'dot':
-        f_input.append(reduce_size)
-    if use_bcast:
-        f_input += [lhs_off, rhs_off]
-        f_name += '_bcast'
-    f_input.append(out)
-    # schedule
-    s = te.create_schedule(out.op)
-    if target == 'llvm':
-        pass
-    elif target == 'cuda':
-        # ntx = find_num_threads(out_len, 1024)
-        # nty = 1024 // ntx
-        # edge_axis, feat_axis = out.op.axis[0], out.op.axis[1]
-        # edge_outer, edge_inner = s[out].split(edge_axis, factor=ntx)
-        # feat_outer, feat_inner = s[out].split(feat_axis, factor=nty)
-        # s[out].bind(feat_outer, te.thread_axis('blockIdx.x'))
-        # s[out].bind(feat_inner, te.thread_axis('threadIdx.x'))
-        # s[out].bind(edge_outer, te.thread_axis('blockIdx.y'))
-        # s[out].bind(edge_inner, te.thread_axis('threadIdx.y'))
-        edge_axis, feat_axis = out.op.axis[0], out.op.axis[1]
-        edge_outer, edge_inner = s[out].split(edge_axis, factor=1024)
-        s[out].bind(edge_outer, te.thread_axis('blockIdx.x'))
-        s[out].bind(edge_inner, te.thread_axis('threadIdx.x'))
-    return tvm.lower(s, f_input, name=f_name)
 
 def sddmm(binary_op, nnz, num_rows, num_cols, 
           lhs_len, rhs_len, out_len, indice_type, feat_type,
@@ -108,10 +25,14 @@ def sddmm(binary_op, nnz, num_rows, num_cols,
         indice_type = 'int32'
     elif '64' in indice_type:
         indice_type = 'int64'
+    else:
+        raise NotImplementedError
     if '32' in feat_type:
         feat_type = 'float32'
     elif '64' in feat_type:
         feat_type = 'float64'
+    else:
+        raise NotImplementedError
     # placeholder for sparse matrix
     adj_row_indices = te.placeholder((nnz,), indice_type, 'adj_row_indices')
     adj_col_indices = te.placeholder((nnz,), indice_type, 'adj_col_indices')
@@ -186,8 +107,11 @@ def sddmm(binary_op, nnz, num_rows, num_cols,
         if binary_op == 'dot' and reduce_size >= 32:
             # if dot product, use tree reduction
             reduce_axis = out.op.reduce_axis[0]
-            eo, ei = s[out].split(edge_axis, factor = (1024 // out_len))
-            s[out].bind(reduce_axis, te.thread_axis('threadIdx.x'))
+            # eo, ei = s[out].split(edge_axis, factor = (1024 // reduce_size))
+            # s[out].bind(reduce_axis, te.thread_axis('threadIdx.x'))
+            ro, ri = s[out].split(reduce_axis, factor = 32)
+            eo, ei = s[out].split(edge_axis, factor = 32)
+            s[out].bind(ri, te.thread_axis('threadIdx.x'))
             s[out].bind(ei, te.thread_axis('threadIdx.y'))
             s[out].bind(eo, te.thread_axis('blockIdx.x'))
         else:
@@ -214,28 +138,6 @@ def sddmm(binary_op, nnz, num_rows, num_cols,
         #     s[out].vectorize(feat_axis)
     # print(tvm.lower(s, f_input))
     return tvm.build(s, f_input, target=target, name=f_name)
-
-def build_all(target, dir = None):
-    # build a .so library packing all kinds of kernel for a target(cuda/llvm)
-    modules = []
-    for op in binary_ops:
-        for id_type in indice_types:
-            for f_type in feature_types:
-                for ub in [True, False]:
-                    # print('Building sddmm_{}_{}_{}{}'.format(op, id_type, f_type, '_bcast' if ub else ''))
-                    modules.append(gsddmm(op, id_type, f_type, ub, target))
-    f = tvm.build(modules, target=target, name='gsddmm_' + target)
-    if dir:
-        f.export_library(dir + '/libgsddmm_tvm_' + target + '.so')
-    return f
-
-# def sddmm_tune(adj_scipy_coo, feat_len, feat_type, binary_op, reduce_size=1, target='llvm'):
-#     nnz = adj_scipy_coo.row.shape[0]
-#     num_rows = adj_scipy_coo.shape[0]
-#     num_cols = adj_scipy_coo.shape[1]
-#     indice_type = str(adj_scipy_coo.row.dtype)
-#     sddmm = autotvm.task.create('sddmm', args=(binary_op, nnz, num_rows, num_cols, feat_len, indice_type, feat_type, reduce_size), target=target)
-#     return sddmm
 
 if __name__ == '__main__':
     target = 'cuda'
--- python/featgraph/module/gspmm.py
+++ python/featgraph/module/gspmm.py
@@ -56,65 +56,6 @@ reduce_ops = ['sum', 'max', 'min']
 indice_types = ['int32', 'int64']
 feature_types = ['float32', 'float64']
 
-def find_num_threads(l, ntx):
-    if ntx <= 1:
-        return 1
-    return te.if_then_else(ntx > l, find_num_threads(l, ntx >> 1), ntx)
-
-def gspmm(binary_op, reduce_op, indice_type='int32', feature_type='float32', use_bcast=False, target='llvm'):
-    # sparse matrix variables
-    nnz = te.var('nnz', 'int64')
-    num_rows = te.var('num_rows', indice_type)
-    num_cols = te.var('num_cols', indice_type)
-    # feature length variables
-    # dtype set to be the same as the other dim in the shape, i.e. nnz, num_rows, num_cols
-    # otherwise cannot pass tvm type check 
-    lhs_len = te.var('lhs_len', indice_type)
-    rhs_len = te.var('rhs_len', 'int64')
-    out_len = te.var('out_len', indice_type)
-    # placeholder for sparse matrix
-    adj_indptr = te.placeholder((num_rows+1,), indice_type, 'adj_indptr')
-    adj_indices = te.placeholder((nnz,), indice_type, 'adj_indices')
-    dst_feat = te.placeholder((nnz, rhs_len), feature_type, 'dst_feat')
-    # placeholder for dense features
-    src_feat = te.placeholder((num_cols, lhs_len), feature_type, 'src_feat')
-    # placeholder for possible broadcasting offset
-    lhs_off = te.placeholder((out_len,), indice_type, 'lhs_off')
-    rhs_off = te.placeholder((out_len,), indice_type, 'rhs_off')
-    # compute
-    def msgfunc(row, fid):
-        row_start = adj_indptr[row]
-        row_end = adj_indptr[row + 1]
-        row_num_elems = row_end - row_start
-        elem_idx = te.reduce_axis((0, row_num_elems), name="elem_idx")
-        adj_val = dst_feat[row_start + elem_idx, rhs_off[fid] if use_bcast else fid]
-        feat_val = src_feat[adj_indices[row_start + elem_idx], lhs_off[fid] if use_bcast else fid]
-        return reduce_op_map[reduce_op](binary_op_map[binary_op](feat_val, adj_val), axis=elem_idx)
-    out = te.compute((num_rows, out_len), msgfunc, name='out')
-    # prepare input
-    f_input = [adj_indptr, adj_indices]
-    if binary_op == 'copy_lhs':
-        f_input.append(src_feat)
-    elif binary_op == 'copy_rhs':
-        f_input.append(dst_feat)
-    else:
-        f_input += [src_feat, dst_feat]
-    f_name = 'spmm_{}_{}_{}_{}'.format(binary_op, reduce_op, indice_type, feature_type)
-    if use_bcast:
-        f_input += [lhs_off, rhs_off]
-        f_name += '_bcast'
-    f_input.append(out)
-    # schedule
-    s = te.create_schedule(out.op)
-    if target == 'llvm':
-        pass
-    elif target == 'cuda':
-        edge_axis, feat_axis = out.op.axis[0], out.op.axis[1]
-        feat_outer, feat_inner = s[out].split(edge_axis, factor=out_len)
-        s[out].bind(feat_outer, te.thread_axis('blockIdx.x'))
-        s[out].bind(feat_inner, te.thread_axis('threadIdx.x'))
-    return tvm.lower(s, f_input, name=f_name)
-
 def spmm(binary_op, reduce_op, nnz, num_rows, num_cols, 
          lhs_len, rhs_len, out_len,
          indice_type, feat_type, use_bcast=False, use_idx=False, target='llvm'):
@@ -122,10 +63,14 @@ def spmm(binary_op, reduce_op, nnz, num_rows, num_cols,
         indice_type = 'int32'
     elif '64' in indice_type:
         indice_type = 'int64'
+    else:
+        raise NotImplementedError
     if '32' in feat_type:
         feat_type = 'float32'
     elif '64' in feat_type:
         feat_type = 'float64'
+    else:
+        raise NotImplementedError
     # placeholder for sparse matrix
     adj_indptr = te.placeholder((num_rows+1,), indice_type, 'adj_indptr')
     adj_indices = te.placeholder((nnz,), indice_type, 'adj_indices')
@@ -321,22 +266,6 @@ def spmm_dds(binary_op, reduce_op, d1_size, d2_size, num_cols,
     f_input.append(out)
     # print(tvm.lower(s, f_input))
     return tvm.build(s, f_input, target=target, name=f_name)
-
-def build_all(target, dir = None):
-    # build a .so library packing all kinds of kernel for a target(cuda/llvm)
-    modules = []
-    for b_op in binary_ops:
-        for r_op in reduce_ops:
-            for id_type in indice_types:
-                for f_type in feature_types:
-                    for ub in [True, False]:
-                        print('Building spmm_{}_{}_{}_{}{}'.format(b_op, r_op, id_type, f_type, '_bcast' if ub else ''))
-                        modules.append(gspmm(b_op, r_op, id_type, f_type, ub, target))
-    f = tvm.build(modules, target=target, name='gspmm_' + target)
-    if dir:
-        f.export_library(dir + '/libgspmm_tvm_' + target + '.so')
-    return f
-
 
 if __name__ == '__main__':
     # import dgl
